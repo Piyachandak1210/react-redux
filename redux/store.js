e2a3d25f2109aa6edee54513c4494f82440178c4
piyachandak1210 <priyankachandak@nimapinfotech.com>
2021-10-08 16:53:50 +0530

    second commit

--- redux/store.js
+++ redux/store.js
@@ -1,7 +1,8 @@
-import {createStore, combineReducers, applyMiddleware} from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import moviesReducer from './reducers';
+
 const rootReducer = combineReducers({
-  moviesReducer,
+    moviesReducer,
 });
 export const store = createStore(rootReducer, applyMiddleware(thunk))
